ae1443baf5d09a314a78d02ee29b6da6ff6c9c3c
Peacefill Amenuveve Tawiah <pamenuveve.ac@st.vvu.edu.gh>
2025-06-06 20:02:20 +0000

    Created product pages

---
+++ app/headphones/xx99-mark-i.tsx
@@ -0,0 +1,13 @@
+import Navbar from '../components/Navbar';
+import Footer from '../components/Footer';
+import Section6 from '../components/Section6';
+
+export default function XX99MarkIPage() {
+  return (
+    <div className="flex flex-col min-h-screen w-full">
+      <Navbar />
+      <Section6 />
+      <Footer />
+    </div>
+  );
+} 
